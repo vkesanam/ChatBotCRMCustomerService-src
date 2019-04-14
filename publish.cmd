f18a98f6ef2a10884f81c3098cef30ff470097e3
VenkataSambasivaRao <sambakesanam@gmail.com>
2019-04-14 15:08:17 +0400

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=chatbotcrmcustomerservice-Web-Deploy.pubxml -p:Password=jfdhmoK1wnYJBsATZmlPHE6vrMkFpkzhXTsT2peH1Y0rlm1BuHoFGhXiCyg7
+
